7a2d6a2a802ff3f214724af877632dfac98f9db7
Dane Stier <DaneStier@u.northwestern.edu>
2014-03-15 01:06:09 -0500

    3/15 Update
    -Changed recipe retrieval to a class
    -Fixed RetrieveTime function
    -Added list of cooking techniques
    -Added ExtractMethod to determine the cooking technique for the
    specific recipe

--- recipes.py
+++ recipes.py
@@ -1,5 +1,7 @@
 import json
 import nltk
+from nltk.stem.lancaster import LancasterStemmer
+st = LancasterStemmer()
 import re, string
 import urllib2
 from BeautifulSoup import BeautifulSoup as Soup
@@ -57,92 +59,201 @@ def getCookware():
     i = i+1
   return cookware
 
-# Open the page from AllRecipes.com given a URL
-def RetrieveInfo():
-  recipe_code = RequestURL()
-  recipe_soup = Soup(recipe_code)
-  recipe_title = ExtractTitle(recipe_soup)
-  recipe_ingred = ExtractIngredients(recipe_soup)
-  recipe_time = ExtractTime(recipe_soup)
-  recipe_dir = ExtractDirections(recipe_soup)
+# Food Preparation Methods
+def getTechniques():
+  i = 0
+  technique_response = urllib2.urlopen('http://chasingdelicious.com/kitchen-101-cooking-methods/')
+  technique_html = technique_response.read()
+  technique_soup = Soup(technique_html)
+  techniques = []
+  for item in technique_soup.findAll('h3'):
+    if i<18:
+      method = item.getText()
+      if '/' in method:
+        method2 = method[method.index('/')+1:]
+        method = method[:method.index('/')]
+        method = st.stem(method)
+        techniques.append(method)
+        method2 = st.stem(method2)
+        techniques.append(method2)
+      else:
+        method = st.stem(method)
+        techniques.append(method)
+    i+= 1
 
-  recipe_info = [recipe_title, recipe_time, recipe_ingred, recipe_dir]
-  return recipe_info
+  return techniques
 
-def RequestURL():
-  print("\n")
-  pageurl = raw_input("Please input the URL of a recipe from AllRecipes.com: ")
-  print("\n")
-  return RetrievePage(pageurl)
 
-def RetrievePage(pageurl):
-  recipes_page = urllib2.urlopen(pageurl)
-  recipes_html = recipes_page.read()
-  return recipes_html
+#-------------------------------------------------------------------------------
+# Class related to the specific recipe.
+#   -Requests URL for recipe
+#   -Retrieves page in HTML
+#   -Parses HTML for specific information (i.e. Title, Cook Time, Ingredients, etc)
+#-------------------------------------------------------------------------------
+class Recipe:
+  def __init__(self):
+    self.pageurl = ""
+    self.recipe_html = ""
+    self.recipe_soup = ""
+    self.title = ""
+    self.ingredients = []
+    self.time = []
+    self.directions = []
+    self.method = ""
+    self.RetrieveInfo()
 
-def ExtractTitle(recipe_soup):
-  listing = recipe_soup.find('span', id="lblTitle")
-  title = listing.getText()
-  return title
+  def RetrieveInfo(self):
+    self.ParsePage()
+    #====retrieve specific items from the page====
+    self.ExtractTitle()
+    self.ExtractIngredients()
+    self.ExtractTime()
+    self.ExtractDirections()
+    self.ExtractMethod(getTechniques())
+    return
+  
+  def ParsePage(self):
+    #====request page URL====
+    print('\n')
+    self.pageurl = raw_input("Please input the URL of a recipe from ALLRecipes.com: ")
+    print('\n')
+    #====convert the page to HTML that can be handled by Beautiful Soup====
+    page = urllib2.urlopen(self.pageurl)
+    self.recipe_html = page.read()
+    self.recipe_soup = Soup(self.recipe_html)
+    return
 
-def ExtractIngredients(recipe_soup):
-  recipe_items = []
-  for listing in recipe_soup.findAll('p', itemprop="ingredients"):
-    amount = listing.find('span', id="lblIngAmount")
-    if amount:
-      amounttext = amount.getText()
-    else:
-      amounttext = ''
-    ingred = listing.find('span', id="lblIngName")
-    if ingred:
-      ingredtext = ingred.getText()
-    else:
-      amounttext = ''
-    recipe_items.append([amounttext, ingredtext])
-  return recipe_items
+  def ExtractTitle(self):
+    listing = self.recipe_soup.find('span', id="lblTitle")
+    self.title = listing.getText()
+    return
 
-def ExtractTime(recipe_soup):
-  listing = recipe_soup.find('span', id="prepMinsSpan")
-  preptime = listing.getText()
-  listing = recipe_soup.find('span', id="cookMinsSpan")
-  cooktime = listing.getText()
-  listing = recipe_soup.find('span', id="totalMinsSpan")
-  totaltime = listing.getText()
-  return [preptime, cooktime, totaltime]
+  def ExtractIngredients(self):
+    for listing in self.recipe_soup.findAll('p', itemprop="ingredients"):
+      amount = listing.find('span', id="lblIngAmount")
+      if amount:
+        amounttext = amount.getText()
+      else:
+        amounttext = ''
+      ingred = listing.find('span', id="lblIngName")
+      if ingred:
+        ingredtext = ingred.getText()
+      else:
+        amounttext = ''
+      self.ingredients.append([amounttext, ingredtext])
+    return
 
-def ExtractDirections(recipe_soup):
-  desc = recipe_soup.find('ol')
-  directions = desc.getText()
+  def ExtractTime(self):
+    #get the prep time minutes and hours
+    listingM = self.recipe_soup.find('span', id="prepMinsSpan")
+    listingH = self.recipe_soup.find('span', id="prepHoursSpan")
+    if listingM and listingH:
+      preptimeM = listingM.getText()
+      preptimeH = listingH.getText()
+      preptime = preptimeH+","+preptimeM
+      self.time.append(preptime)
+    elif listingM:
+      preptimeM = listingM.getText()
+      self.time.append(preptimeM)
+    elif listingH:
+      preptimeH = listingH.getText()
+      self.time.append(preptimeH)
+    #get the cook time minutes and hours
+    listingM = self.recipe_soup.find('span', id="cookMinsSpan")
+    listingH = self.recipe_soup.find('span', id="cookHoursSpan")
+    if listingM and listingH:
+      cooktimeM = listingM.getText()
+      cooktimeH = listingH.getText()
+      cooktime = cooktimeH+","+cooktimeM
+      self.time.append(cooktime)
+    elif listingM:
+      cooktimeM = listingM.getText()
+      self.time.append(cooktimeM)
+    elif listingH:
+      cooktimeH = listingH.getText()
+      self.time.append(cooktimeH)
+    #get the total time minutes and hours
+    listingM = self.recipe_soup.find('span', id="totalMinsSpan")
+    listingH = self.recipe_soup.find('span', id="totalHoursSpan")
+    if listingM and listingH:
+      totaltimeM = listingM.getText()
+      totaltimeH = listingH.getText()
+      totaltime = totaltimeH+","+totaltimeM
+      self.time.append(totaltime)
+    elif listingM:
+      totaltimeM = listingM.getText()
+      self.time.append(totaltimeM)
+    elif listingH:
+      totaltimeH = listingH.getText()
+      self.time.append(totaltimeH)
+    return
 
-  sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-  directions = sent_detector.tokenize(directions)
-  return directions
+  def ExtractDirections(self):
+    desc = self.recipe_soup.find('ol')
+    directions = desc.getText()
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    self.directions = sent_detector.tokenize(directions)
+    return
 
-def PrintInfo(recipe_info):
-  print("\n####" + recipe_info[0] + "####" + '\n')
-  print("==Recipe Time==")
-  print("  Prep Time:  " + recipe_info[1][0])
-  print("  Cook Time:  " + recipe_info[1][1])
-  print("  Total Time: " + recipe_info[1][2] + '\n')
+  def ExtractMethod(self, methodlist):
+    for sentence in self.directions:
+      sentence = sentence.split()
+      for token in sentence:
+        if '.' in token:
+          index = token.index('.')
+          token2 = token[index+1:]
+          temptoken2 = st.stem(token2)
+          token = token[:index]
+          temptoken = st.stem(token)
+          if unicode(temptoken2) in methodlist:
+            self.method = token2
+            return
+        else:
+          temptoken = st.stem(token)
+        if unicode(temptoken) in methodlist:
+          self.method = token
+          return
 
-  print("==Ingredients==")
-  for item in recipe_info[2]:
-    print("  " + item[1] + " (" + item[0] +")")
-  print("\n==Directions==")
-  for sentence in recipe_info[3]:
-    print("-" + sentence)
-  print('\n')
-  return
+  def Print(self):
+    print("\n####" + self.title + "####" + '\n')
+    print("\n#==========================================#")
+    print("#  Recipe Time")
+    print("#==========================================#")
+    print("-->Prep Time:  " + self.time[0])
+    print("-->Cook Time:  " + self.time[1])
+    print("-->Total Time: " + self.time[2] + '\n')
+    print("\n#==========================================#")
+    print("#  Ingredients")
+    print("#==========================================#")
+    for item in self.ingredients:
+      print("-->" + item[1] + " (" + item[0] + ")")
+    print("\n#==========================================#")
+    print("#  Directions")
+    print("#==========================================#")
+    i = 1
+    for sentence in self.directions:
+      print("  "+str(i) + ".) " + sentence)
+      i+= 1
+    print("\n#==========================================#")
+    print("#  Preparation Technique")
+    print("#==========================================#")
+    print("-->" + self.method + '\n')
+    return
 
+
+#-------------------------------------------------------------------------------
+# Calling the functions defined about
+#-------------------------------------------------------------------------------
 utensils = getUtensils()
 cookware = getCookware()
-recipe = RetrieveInfo()
+recipe = Recipe()
+#recipe.RetrieveInfo()
+recipe.Print()
 
 print("Utensils:")
 print utensils
 print('\nCookware:')
 print cookware
-PrintInfo(recipe)
 
 
 
